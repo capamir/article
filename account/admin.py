3ffc7a945d3a83743c4806dde0e0e888f428a73d
amirhossein <79211332+capamir@users.noreply.github.com>
2023-06-28 19:45:31 +0330

    otp code model

--- account/admin.py
+++ account/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
-from .models import User
+from .models import User, OtpCode
 from .forms import UserCreationForm, UserChangeForm
 
 # Register your models here.
+@admin.register(OtpCode)
+class OtpCodeAdmin(admin.ModelAdmin):
+	list_display = ('phone_number', 'code', 'created')
+
 
 class UserAdmin(BaseUserAdmin):
 	form = UserChangeForm
--- account/models.py
+++ account/models.py
@@ -38,3 +38,12 @@ class User(AbstractBaseUser):
 
 	def has_module_perms(self, app_label):
 		return True
+
+
+class OtpCode(models.Model):
+	phone_number = models.CharField(max_length=11, unique=True)
+	code = models.PositiveSmallIntegerField()
+	created = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return f'{self.phone_number} - {self.code} - {self.created}'
