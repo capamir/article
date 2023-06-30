9483abf77458249932bec6e52ccd7635804350c2
bnparham <parhambn.workmail@gmail.com>
2023-06-30 14:06:37 +0430

    filter owner users in article admin

--- articles/admin.py
+++ articles/admin.py
@@ -8,6 +8,12 @@ class ArticleAdmin(admin.ModelAdmin):
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ["owner", "article","created"]
+    
+    def get_queryset(self, request):
+        # Filtering by specific role
+        queryset = super().get_queryset(request)
+        queryset = queryset.filter(owner__role='proffesor')
+        return queryset
 
 admin.site.register(ArticleModel, ArticleAdmin)
 admin.site.register(ReviewModel, ReviewAdmin)
