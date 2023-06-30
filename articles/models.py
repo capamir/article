15c98f31d630351ccf335d5c98683f5d8e3b6ac1
bnparham <parhambn.workmail@gmail.com>
2023-06-30 13:07:20 +0430

    add ReviewModel in article_module

--- articles/models.py
+++ articles/models.py
@@ -7,3 +7,9 @@ class ArticleModel(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=500)
     file = models.FileField(upload_to='articles')
+
+class ReviewModel(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(ArticleModel, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
