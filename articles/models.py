168a3117e5f988efe0db860006826f135004f155
bnparham <parhambn.workmail@gmail.com>
2023-06-30 13:01:53 +0430

    add ArticleModel in article_module

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from account.models import User
 
 # Create your models here.
+class ArticleModel(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    file = models.FileField(upload_to='articles')
