d704602361b7b9c20b464cf962b2b9cc42ee029c
bnparham <parhambn.workmail@gmail.com>
2023-06-30 13:13:40 +0430

    add date field to article model

--- articles/models.py
+++ articles/models.py
@@ -7,9 +7,16 @@ class ArticleModel(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=500)
     file = models.FileField(upload_to='articles')
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
 
 class ReviewModel(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     article = models.ForeignKey(ArticleModel, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.owner} - {self.article.title}"
