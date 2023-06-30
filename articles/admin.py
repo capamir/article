1bfdfd30f47d2227fa4d1828d46365ef301b8f7e
amirhossein <79211332+capamir@users.noreply.github.com>
2023-06-30 15:38:17 +0330

    some aditional feature to admin panel

--- articles/admin.py
+++ articles/admin.py
@@ -5,9 +5,16 @@ from .models import *
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ["user", "title", "created"]
+    raw_id_fields = ('user',)
+    search_fields = ('title',)
+    ordering = ('created',)
+    readonly_fields = ('title', 'description')
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ["owner", "article","created"]
+    list_display = ["owner", "article", "created"]
+    raw_id_fields = ('owner', 'article')
+    ordering = ('created',)
+
 
 admin.site.register(ArticleModel, ArticleAdmin)
 admin.site.register(ReviewModel, ReviewAdmin)
