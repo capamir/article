9ca503ad9b8aa2aa91197de1296782b386671d8d
bnparham <parhambn.workmail@gmail.com>
2023-06-30 13:10:37 +0430

    add article models to admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["user", "title"]
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["owner", "article","created"]
+
+admin.site.register(ArticleModel, ArticleAdmin)
+admin.site.register(ReviewModel, ReviewAdmin)
