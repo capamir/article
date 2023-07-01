542fdb4f77a8ef7b498d5e24831280586c67c371
bnparham <parhambn.workmail@gmail.com>
2023-07-02 00:26:29 +0430

    move article signals to signals.py

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from account.models import User, Professor
 import uuid
-from django.db import transaction
+
 
 # Create your models here.
 class Article(models.Model):
     owner = models.ForeignKey(User,on_delete=models.CASCADE)
-    judges = models.ManyToManyField(Professor, related_name='articles', null=True, default=None, blank=True)
+    judges = models.ManyToManyField(Professor, related_name='articles',default=None,blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=500)
     file = models.FileField(upload_to='articles')
@@ -35,32 +35,3 @@ class Notification_Manager(models.Model):
      
     def __str__(self):
         return self.article.title
-    
-# ===== signals ===== 
-from django.dispatch import receiver
-from django.db.models.signals import post_save,pre_save
-
-from .models import Article,Review,Notification_Manager
-
-@receiver(pre_save, sender=Article)
-def article_post_save_handler(sender, instance:Article, *args, **kwargs):
-    pass
-
-@receiver(post_save, sender=Article)
-def article_post_save_handler(sender, instance:Article, created, *args, **kwargs):
-    if created:
-        # create notification that new article added
-        new_notife = Notification_Manager.objects.create(article=instance)
-        new_notife.save()
-    else:
-        # create review rows for judge
-        get_judge_list = instance.judges.all()
-        if get_judge_list is not None:
-            with transaction.atomic():
-                for judge in get_judge_list:
-                    judge:Professor = judge 
-                    get,c = Review.objects.get_or_create(owner=judge, article=instance)
-                    if(c):
-                        get.save()    
-        else:
-            pass
--- articles/signals.py
+++ articles/signals.py
@@ -1,20 +1,28 @@
-# from django.dispatch import receiver
-# from django.db.models.signals import post_save
+# ===== signals ===== 
+from django.dispatch import receiver
+from django.db.models.signals import post_save,pre_save
+from django.db import transaction
+from account.models import User, Professor
 
-# from .models import Article,Review,Notification_Manager
+from .models import Article,Review,Notification_Manager
 
-# @receiver(post_save, sender=Article)
-# def article_post_save_handler(sender, instance:Article, created, *args, **kwargs):
-#     if created:
-#         # create notification that new article added
-#         new_notife = Notification_Manager.objects.create(article=sender)
-#         new_notife.save()
-#     else:
-#         # create review rows for judge
-#         pass
-#         # get_judge_list = instance.judges.all()
-#         # get_user = instance.owner
-#         # li = []
-        
-#         # for judge in get_judge_list:
-#         #     pass
+@receiver(pre_save, sender=Article)
+def article_pre_save_handler(sender, instance:Article, *args, **kwargs):
+    print("pre",instance.judges.all())
+
+@receiver(post_save, sender=Article)
+def article_post_save_handler(sender, instance:Article, created, *args, **kwargs):
+    if created:
+        # create notification that new article added
+        new_notife = Notification_Manager.objects.create(article=instance)
+        new_notife.save()
+    else:
+        # create review rows for judge
+        print("post",instance.judges.all())
+        # if get_judge_list:
+        #     with transaction.atomic():
+        #         for judge in get_judge_list:
+        #             judge:Professor = judge 
+        #             get,c = Review.objects.get_or_create(owner=judge, article=instance)
+        #             if(c):
+        #                 get.save()
