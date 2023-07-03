b9e1589fca83e76004507890bc856ff2269d874d
bnparham <parhambn.workmail@gmail.com>
2023-07-03 15:17:21 +0430

    comment caches setting

--- A/settings.py
+++ A/settings.py
@@ -87,12 +87,12 @@ DATABASES = {
 }
 
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379',
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+#         'LOCATION': 'redis://127.0.0.1:6379',
+#     }
+# }
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
