b051c96c5075fe55445ce62e371479129ac735a4
bnparham <parhambn.workmail@gmail.com>
2023-07-05 02:18:38 +0430

    alter article model with created and admin_last_view field, update signals.py and admin.py

--- articles/signals.py
+++ articles/signals.py
@@ -7,6 +7,9 @@ from datetime import datetime
 
 from .models import Article,Review
 
+
+def article_pre_save_handeler(sender, instance:Article, *args, **kwargs):
+    instance.admin_last_view = datetime.now()
         
 @receiver(m2m_changed, sender=Article.judges.through)
 def articel_update_when_judge_added(sender, instance:Article, action, *args, **kwargs):
@@ -17,8 +20,8 @@ def articel_update_when_judge_added(sender, instance:Article, action, *args, **k
             with transaction.atomic():
                 for judge in get_judge_list:
                     judge:Professor = judge 
-                    get,c = Review.objects.get_or_create(owner=judge, article=instance)
-                    if(c):
+                    get,create = Review.objects.get_or_create(owner=judge, article=instance)
+                    if(create):
                         get.save()
                         
 
@@ -26,7 +29,6 @@ def review_post_save_handler(sender, instance:Review, created, *args, **kwargs):
     if created:
         find_article = instance.article
         find_article.is_view = True
-        find_article.last_view = datetime.now()
         find_article.save()
             
         
@@ -36,8 +38,10 @@ def review_post_delete_handeler(sender, instance:Review, *args, **kwargs):
     find_article.judges.remove(find_judge)
     if(find_article.judges.exists() == False):
         find_article.is_view = False
-    find_article.save()
+    # save article with out send signal,
+    Article.objects.bulk_update([find_article,],fields=["is_view"])
 
 
 post_save.connect(review_post_save_handler, sender=Review)
 post_delete.connect(review_post_delete_handeler, sender=Review)
+pre_save.connect(article_pre_save_handeler, sender=Article)
