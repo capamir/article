1b9dc42b271d4ffff2ebaf289de541f3ba31daf4
bnparham <parhambn.workmail@gmail.com>
2023-07-04 21:32:06 +0430

    add render partial for student view

--- userPanel_module/views.py
+++ userPanel_module/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from  django.views import View
 # Create your views here.
+
+class StudentView(View):
+    def get(self, request):
+        context = {}
+        return render(request, 'userPanel_module/studentView.html', context)
+    def post(self, request):
+        pass
