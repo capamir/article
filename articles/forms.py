1e6ca9ceaffa7ef27d98165f312b427e0cf4014b
bnparham <parhambn.workmail@gmail.com>
2023-07-04 21:07:57 +0430

    add article form

---
+++ articles/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from  .models import Article,Review
+
+class ArticleForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(ArticleForm, self).__init__(*args, **kwargs)
+
+    title = forms.CharField(
+        label="Title",
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "id": "article_title",
+            }
+        ),
+    description = forms.CharField(
+        label="Title",
+        max_length=500,
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "id": "article_description",
+            }
+        ),
+    file = forms.FileField(
+        label="Title",
+        max_length=100,
+        widget=forms.FileInput(
+            attrs={
+                "class": "form-control",
+                "id": "article_file",
+            }
+        ),
+
+    )
