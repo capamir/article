c6531ad7f08dc7fc09cd6efacb8b040544b6c9c0
bnparham <parhambn.workmail@gmail.com>
2023-07-04 22:43:23 +0430

    add article_list_view section

--- userPanel_module/views.py
+++ userPanel_module/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from  django.views import View
 # Create your views here.
+from django.views.generic import TemplateView,ListView
+from articles.models import Article
 
-class StudentView(View):
-    def get(self, request):
-        context = {}
-        return render(request, 'userPanel_module/studentView.html', context)
-    def post(self, request):
-        pass
+class StudentView(TemplateView):
+    template_name = 'userPanel_module/student_view/studentView.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentView, self).get_context_data()
+        context['profile'] = self.request.user.profile
+        return context
+
+class StudentArticlesView(ListView):
+    model = Article
+    template_name = 'userPanel_module/student_view/Article_list_view.html'
+
+    def get_queryset(self):
+        query = super(StudentArticlesView, self).get_queryset()
+        return query
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(StudentArticlesView, self).get_context_data()
+        user = self.request.user
+        articles = Article.objects.filter(owner=user)
+        context['articles'] = articles
+        return context
