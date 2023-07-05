7e92f99d972957f1ef4c5e5bf0e9009dea7b26be
bnparham <parhambn.workmail@gmail.com>
2023-07-05 21:22:57 +0430

    fix userProfile image url

--- account/models.py
+++ account/models.py
@@ -46,8 +46,7 @@ class Profile(models.Model):
     location = models.CharField(max_length=200, blank=True, null=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-    image = models.ImageField(
-        null=True, blank=True , default="profiles/user-default.png")
+    image = models.ImageField(upload_to='profiles', default=None)
     social_github = models.CharField(max_length=200, blank=True, null=True)
     social_twitter = models.CharField(max_length=200, blank=True, null=True)
     social_linkedin = models.CharField(max_length=200, blank=True, null=True)
