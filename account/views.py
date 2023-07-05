76e4a30bdd797b7289b2bc2e3d638551c2d25d61
bnparham <parhambn.workmail@gmail.com>
2023-07-05 11:59:21 +0430

    redirect to account profile after login

--- account/views.py
+++ account/views.py
@@ -112,7 +112,7 @@ class UserLoginView(View):
 			if user is not None:
 				login(request, user)
 				messages.success(request, 'you logged in successfully', 'info')
-				return redirect('articles:articles')
+				return redirect('account:account')
 			messages.error(request, 'phone or password is wrong', 'warning')
 		return render(request, self.template_name, {'form':form})
 
